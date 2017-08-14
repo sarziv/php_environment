132ce911aeccac48181238ca33759600e251fcd1
sarziv <saruzivila12@gmail.com>
2017-08-14 22:50:53 +0300

    search module done
    search by name added

--- sites/html/Php_mysql/MarketWebsite/search.php
+++ sites/html/Php_mysql/MarketWebsite/search.php
@@ -11,10 +11,61 @@
 <div id="page-content-wrapper">
             <div class="container-fluid">
                 <div class="row">
-		<?php include 'button/buttonMenu.html';?>
-			<div class="padd" >
-		<?php include 'content/cont_search_1.html';?>
-			</div>
+		<?php include 'button/buttonMenuCp.html';?>
+			<div
+<?php
+	require 'backend/connect.php';
+	$conn    = Connect();
+	
+if (isset($_POST['products'])) {
+	$products    = $conn->real_escape_string($_POST['products']);
+}
+echo "$products";
+$query   = "
+SELECT * FROM products WHERE products LIKE '%{$products}%'";
+
+	$success = $conn->query($query);
+	
+if (!$success) {
+    die("Couldn't enter data: ".$conn->error);
+}
+
+	$result = $conn->query($query);
+echo "$products";
+?>
+
+<h3 class="text-center">Search</h3>
+<h2><?php echo $products ?></h2>
+<table class="table table-striped">                     
+    <div class="table responsive">
+        <thead>
+            <tr>
+				<th>ID</th>
+              <th>Products</th>
+              <th>Price</th>
+            </tr>
+        </thead>
+    <tbody>
+
+<?php 
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo '<tr>
+                  <td scope="row">' 
+						. $row["id"]. '</td>
+						<td> '.$row["products"] .'</td>
+                  <td> '.$row["price"] .'</td>
+                </tr>';
+    }
+} else {
+    echo "0 results";
+} 
+?>
+       </tbody>
+    </div>
+	</div>
                 </div>
             </div>
         </div>
--- sites/html/Php_mysql/MarketWebsite/searchPanel.php
+++ sites/html/Php_mysql/MarketWebsite/searchPanel.php
@@ -1,19 +1,20 @@
 <html>
-<?php include '.head.html';?>
+<?php include 'head.html';?>
 <body>
 		
 <div id="wrapper">
 
 <!-- Sidebar -->
-		<?php include '.sidePanel/menu.html';?>
+		<?php include 'sidePanel/menu.html';?>
 <!-- /#sidebar-wrapper -->
 
 <div id="page-content-wrapper">
             <div class="container-fluid">
                 <div class="row">
 		<?php include 'button/buttonMenuCp.html';?>
-			<div class="padd" 
-			
+
+			<div class="padd" >
+		<?php include 'content/cont_search_1.html';?>
 			</div>
                 </div>
             </div>
